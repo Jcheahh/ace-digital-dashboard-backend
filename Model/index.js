0d7ebd7b12e6a30cdb405ae0c613f62a69de204e
jack <jackcheah2@gmail.com>
2022-11-08 11:45:32 +0800

    Connect to database

---
+++ Model/index.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  `postgres://dashboard:dashboard@localhost:5432/dashboard`,
+  {
+    dialect: "postgres",
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(`Database connected to discover`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require("./userModel")(sequelize, DataTypes);
+
+module.exports = db;
