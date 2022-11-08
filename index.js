17e90d0ca4301f454b2d7289b88c721e61eb5506
jack <jackcheah2@gmail.com>
2022-11-08 23:46:56 +0800

    Routes works

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const port = 8000;
+const cookieParser = require("cookie-parser");
+const db = require("./Model");
+const userRoutes = require("./Routes/userRoutes");
+const carRoutes = require("./Routes/carRoutes");
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
 });
+
+db.sequelize.sync().then(() => {
+  console.log("db has been re sync");
+});
+
+app.use("/api/users", userRoutes);
+
+app.use("/api/cars", carRoutes);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
