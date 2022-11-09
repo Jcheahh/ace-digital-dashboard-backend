158370e8beaaa33dae93e294816648fe06181ebd
jack <jackcheah2@gmail.com>
2022-11-09 22:31:45 +0800

    Add cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const db = require("./Model");
+const cors = require("cors");
+
 const userRoutes = require("./Routes/userRoutes");
 const carRoutes = require("./Routes/carRoutes");
 
@@ -8,6 +10,7 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
